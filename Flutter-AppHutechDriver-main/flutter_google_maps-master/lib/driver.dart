7cef61d797d5621952f4f32f6e55caa4d33b7480
DuyyAnh <tranduyanh2174@gmail.com>
2023-11-26 20:40:50 +0700

    Initial commit

---
+++ Flutter-AppHutechDriver-main/flutter_google_maps-master/lib/driver.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_google_maps/detailTrip.dart';
+import 'package:flutter_google_maps/signIn.dart';
+import 'package:flutter_google_maps/token.dart';
+import 'package:flutter_google_maps/trip.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
+class DriverPage extends StatefulWidget {
+  @override
+  _DriverPageState createState() => _DriverPageState();
+}
+class _DriverPageState extends State<DriverPage> {
+  List<Trip> trips = [];
+  String selectedStatus = 'Chưa nhận';
+  int driverId = 0;
+
+  // Map to group statuses together
+  Map<String, List<String>> statusGroups = {
+    'Chưa nhận': ['Chưa nhận'],
+    'Đã nhận': ['Đã nhận đơn','Đang chạy', 'Hoàn thành'], // Add other statuses you want to include
+  };
+
+  // Filtered trips based on selected status
+ List<Trip> get filteredTrips {
+  return selectedStatus == 'Chưa nhận'
+      ? trips.where((trip) => trip.status == 'Chưa nhận').toList()
+      : trips.where((trip) => trip.status != 'Chưa nhận' && trip.driverId == driverId).toList();
+}
+
+  @override
+  void initState() {
+    super.initState();
+    getAllTrip();
+    decodetoken(TokenManager.getToken());
+  }
+  Future<void> getDetailTrip(int id) async {
+    final response = await http.get(
+      Uri.parse('https://10.0.2.2:7145/api/Trip/GetDetailTrip?id=$id'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DetailTripPage(tripId: id)));
+    }
+  }
+  Future<void> decodetoken(String Token) async {
+ 
+  final response = await http.post(
+    Uri.parse('https://10.0.2.2:7145/api/Auth/DecodeToken?token=$Token'),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    setState(() {
+      driverId = int.parse(responseData['userId']);
+    });
+  } else {
+     debugPrint("Error: ${response.statusCode}");
+     debugPrint("Response body: ${response.body}");
+  }
+  }
+  Future<void> getAllTrip() async {
+    final response = await http.get(
+      Uri.parse('https://10.0.2.2:7145/api/Trip/GetAllTrips'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> tripData = json.decode(response.body);
+      setState(() {
+        trips = tripData.map((data) => Trip.fromJson(data)).toList();
+      });
+    }
+  }
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => LoginPage()));
+          },
+          icon:Icon(Icons.arrow_back_ios),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.all(100.0),
+          child: Text('Driver'),
+
+        ),
+      ),
+      body: Column(
+        children: [
+         Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: statusGroups.keys.map((status) {
+            return ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  selectedStatus = status;
+                });
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: status == selectedStatus ? Colors.blue : null,
+                ),
+                child: Text(status),
+              );
+            }).toList(),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredTrips.length,
+              itemBuilder: (context, index) {
+                final trip = filteredTrips[index];
+                final timeBookString = trip.timeBook;
+                final timeBookDateTime = DateTime.parse(timeBookString);
+                final formattedDateTime = DateFormat('dd-MM-yyyy HH:mm').format(timeBookDateTime);
+                final startLocation = trip.startLocation.substring(0, 16);
+                final endLocation = trip.endLocation.substring(0, 16);
+
+                return Card(
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Chuyến đi lúc $formattedDateTime', style: TextStyle(fontSize: 20)),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('$startLocation - $endLocation, style: TextStyle(fontSize: 20)'),
+                        Text('Giá: ${trip.price} đ', style: TextStyle(fontSize: 20)),
+                      ],
+                    ),
+                    trailing: Icon(Icons.arrow_forward),
+                    onTap: () async {
+                      await getDetailTrip(trip.tripId);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Flutter-AppHutechDriver-main/flutter_google_maps-master/lib/signIn.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/gestures.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_google_maps/forgotPassword.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_google_maps/main.dart';
+import 'package:flutter_google_maps/driver.dart';
+import 'package:flutter_google_maps/register.dart';
+import 'package:flutter_google_maps/token.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+String userRole = "";
+
+Future<void> decodetoken(String Token) async {
+ 
+  final response = await http.post(
+    Uri.parse('https://10.0.2.2:7145/api/Auth/DecodeToken?token=$Token'),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    userRole = responseData['role'];
+ 
+  } else {
+     debugPrint("Error: ${response.statusCode}");
+     debugPrint("Response body: ${response.body}");
+  }
+  }
+
+  Future<void> login() async {
+  final Map<String, dynamic> data = {
+    'userName': usernameController.text,
+    'passWord': passwordController.text,
+  };
+
+  final response = await http.post(
+    Uri.parse('https://10.0.2.2:7145/api/Auth/Login'),
+    body: jsonEncode(data), // Chuyển đổi dữ liệu thành JSON
+    headers: {
+      'Content-Type': 'application/json', // Đặt header Content-Type thành application/json
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // Xử lý đăng nhập thành công
+   final Map<String, dynamic> responseData = json.decode(response.body);
+   if (responseData['token'] != null) {
+      TokenManager.setToken(responseData['token']);
+      decodetoken(responseData['token']);
+      if (userRole == "Member") {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyApp()));
+    } else if (userRole == "Driver") {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DriverPage()));
+    }
+  } else {
+     debugPrint("Error: ${response.statusCode}");
+     debugPrint("Response body: ${response.body}");
+  }
+  }
+}
+bool isObscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Đăng nhập')),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 140,
+              ),
+              Image.asset('assets/image/ic_car_green.png'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 6),
+                child: Text(
+                  'Welcome back!',
+                  style: TextStyle(fontSize: 22, color: Color(0xff333333)),
+                ),
+              ),
+              Text(
+                'Login to continue using iCab',
+                style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 80, 0, 20),
+                child: TextField(
+                  controller: usernameController,
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset('assets/image/ic_mail.png'),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color(0xffCED0D2),
+                              width: 1
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(6))
+                      )
+                  ),
+                ),
+              ),
+              Stack(
+                alignment: AlignmentDirectional.centerEnd,
+                children: [
+                  TextField(
+                    controller: passwordController,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black
+                    ),
+                    obscureText: isObscurePassword,
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                        prefixIcon: Container(
+                          width: 50,
+                          child: Image.asset('assets/image/ic_lock.png'),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(0xffCED0D2),
+                                width: 1
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(6))
+                        )
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      setState(() {
+                        isObscurePassword = !isObscurePassword;
+                      });},
+                    icon: Icon(Icons.remove_red_eye, color: Colors.grey,),
+                  )
+                ],
+              ),
+            GestureDetector(
+              onTap: () {
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+              );
+              },
+              child: Container(
+              constraints: BoxConstraints.loose(Size(double.infinity, 30)),
+              alignment: AlignmentDirectional.centerEnd,
+              child: Text(
+              'Forgot password?',
+              style: TextStyle(fontSize: 16, color: Color(0xff3277D8)),
+              ),
+              ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: ElevatedButton(
+                    onPressed:login,
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+               Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                    text: TextSpan(
+                        text: 'New user? ',
+                        style: TextStyle(color: Color(0xff606470), fontSize: 16),
+                        children: <TextSpan>[
+                          TextSpan(
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()));
+                                },
+                              text: 'Sign up for a new account',
+                              style: TextStyle(
+                                  color: Color(0xff3277D8), fontSize: 16
+                              )
+                          )
+                        ]
+                    )
+                ),
+              )
+            ]
+          ),
+      ),
+      )
+      );
+  }
+}
